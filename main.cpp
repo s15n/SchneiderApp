59bc1a9589d5fc2ecb8998a40c35478d9cdf1f52
FauxKiwi <sfn14ok04@gmail.com>
2020-11-18 11:03:53 +0100

    Now it's working

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
 
+#include "fparser4.5.2/fparser.hh"
+
 int main() {
+    FunctionParser fp;
+    fp.Parse("x+y", "x,y");
+    double variables[2] = { 1.5, 2.9 };
+    double result = fp.Eval(variables);
+    std::cout << result << std::endl;
     return 0;
 }
